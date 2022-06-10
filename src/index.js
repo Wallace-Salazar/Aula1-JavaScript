8b48b8df7f7abdb684b320d778207f7f25da20b7
Wallace Oliveira <104787895+Wallace-Salazar@users.noreply.github.com>
2022-06-10 15:59:26 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,68 @@
+console.log("Olá, Mundo!");
+console.log(3);
+console.log(2);
+
+/*----------Declarar Variaveis----------*/
+
+//var - declara uma variavel de escopo global , quase não usado
+//let - declara uma variavel de escopo local, é a mais usada
+//const - declara uma constante.
+
+let aniversario = 1998; // = 'recebe'
+let nomeVariavel = "atribuição";
+
+/* let _nome
+let $nome */
+
+/*---------------Tipos dados--------------*/
+
+let string = "Lorena";
+let string2 = "da Silva";
+console.log(string);
+console.log(string2);
+
+let templateString = `o Nome dela é ${string} ${string2}`;
+console.log(templateString);
+
+let number = 10;
+console.log(number);
+let nome = "10";
+console.log(nome);
+
+/* let boolean = false //dados do tipo verddeiro e falso
+console.log(`o valor de boolean é ${boolean}`) */
+
+/* let nome = true
+console.log(`o nome da thayssa é ${nome}`) */
+
+/* let dado = null //dado nulo ou não existente
+console.log(dado) */
+
+/* let dado //dado undefined é um dado que não atribuido valor algum
+console.log(dado) */
+
+/* let dado = NaN // não é um dado numerico  */
+
+/* console.log('oi "to aqui"')
+console.log("oi 'to aqui'") */
+
+/* let nome = 'Rhuan'
+console.log(nome)
+nome = 'Thais'
+console.log(nome) */
+
+/* let nome = 'Thais'
+let sobrenome = 'da silva'
+console.log(`o nome e o sobrenome dela é ${nome} ${sobrenome}`) */
+
+/* let nome
+console.log(nome)
+nome = "Stéfany"
+console.log(nome) */
+
+/* let jogarbola = '10'
+ let number = 10
+ let nome = false
+console.log(typeof jogarbola) // tipo de dado que eu uso na variavel que foi chamada
+console.log(typeof number)
+console.log(typeof nome) */
